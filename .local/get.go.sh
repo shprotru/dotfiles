aad5e87c3fcb86466e0badac1421fdf71878efdd
Pavel Sereda <pavel.sereda@altkraft.com>
2020-11-15 12:22:39 +0300

    get go through docker

--- .local/get.go.sh
+++ .local/get.go.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-goversion=1.14.4
-wget https://dl.google.com/go/go${goversion}.linux-amd64.tar.gz
-tar xvf go${goversion}.linux-amd64.tar.gz
-rm go${goversion}.linux-amd64.tar.gz
+#goversion=1.14.4
+#wget https://dl.google.com/go/go${goversion}.linux-amd64.tar.gz
+#tar xvf go${goversion}.linux-amd64.tar.gz
+#rm go${goversion}.linux-amd64.tar.gz
+
+if [ -x "$(command -v go)" ]; then
+	echo 'you are ready to go'
+else
+	export GO_DEST=$HOME/.local/go
+	docker pull golang
+	mkdir -p $GO_DEST
+	docker run --mount type=volume,dst=/usr/local/go,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$GO_DEST golang
+	sudo chown -R $USER:$USER $GO_DEST
+	export GOROOT=$HOME/.local/go
+	export PATH=$PATH:$GOROOT/bin
+fi
 
